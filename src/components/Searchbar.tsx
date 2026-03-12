89a5032be9824dc175f64635fe0ddc61f997c8c5
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-03-12 20:25:31 +0100

    searchbar

---
+++ src/components/Searchbar.tsx
@@ -0,0 +1,12 @@
+import { Search } from "lucide-react";
+
+const Searchbar = () => {
+  return (
+    <label className="bg-light-gray text-dark-gray flex gap-2 p-2.5 rounded-sm items-center w-full">
+      <Search className="size-3 text-dark-gray" />
+      <input type="text" placeholder="Search..." className="outline-none" />
+    </label>
+  );
+};
+
+export default Searchbar;
