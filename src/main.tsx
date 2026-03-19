5d4d33b4573e170d8c2e2527b73ea68fc60dc80a
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-03-19 19:38:08 +0100

    add layouts and providers to application

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import QueryLayout from "./Layout/QueryLayout.tsx";
+import { BrowserRouter } from "react-router-dom";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryLayout>
+      <BrowserRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
+    </QueryLayout>
   </StrictMode>,
-)
+);
