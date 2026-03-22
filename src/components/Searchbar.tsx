54760c3dd223275f4f6d1b25d5ae105bfc8f0586
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-03-22 22:33:00 +0100

    corrected styles

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -2,8 +2,8 @@ import { Search } from "lucide-react";
 
 const Searchbar = () => {
   return (
-    <label className="bg-light-gray text-dark-gray flex gap-2 p-2.5 rounded-sm items-center w-full">
-      <Search className="size-3 text-dark-gray" />
+    <label className="bg-(--light-gray) text-(--dark-gray) flex gap-2 p-2.5 rounded-sm items-center w-full">
+      <Search className="size-3 text-(--dark-gray)" />
       <input type="text" placeholder="Search..." className="outline-none" />
     </label>
   );
