234aa30497058f3ce2e85174494217a4bec7228c
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-03-22 22:28:00 +0100

    add selectors for user names

--- features/user/selectors.ts
+++ features/user/selectors.ts
@@ -1,5 +1,9 @@
 import type { RootState } from "../../src/app/store";
 
 export const userRole = (state: RootState) => state.user.user.role;
+export const userDisplayName = (state: RootState) =>
+  state.user.user.displayName;
+export const userFirstName = (state: RootState) => state.user.user.firstName;
+export const userLastName = (state: RootState) => state.user.user.lastName;
 export const userLoggedIn = (state: RootState) => state.user.isLoggedIn;
 export const userLoading = (state: RootState) => state.user.isLoading;
