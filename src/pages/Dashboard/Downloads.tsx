d5cb777eb8547ea2f28ec499754a710c2357ba80
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-05-08 12:59:05 +0200

    download component

--- src/pages/Dashboard/Downloads.tsx
+++ src/pages/Dashboard/Downloads.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import Section from "../../components/Section";
+import NoInList from "../../components/Dashboard/NoInList";
+import { useQuery } from "@tanstack/react-query";
+import { getDownloads } from "../../../features/download/request";
+import DownloadList from "../../components/Dashboard/Download/DownloadList";
 
 const Downloads = () => {
-  return <div>Downloads</div>;
+  // const downloads = [];
+
+  const { data: downloads = [], isFetching } = useQuery({
+    queryKey: ["getData"],
+    queryFn: getDownloads,
+  });
+
+  return (
+    <Section changePaddings="min-[1024px]:px-24">
+      {isFetching ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          {downloads.length === 0 ? (
+            <NoInList link="/" message="No Download has been made yet." />
+          ) : (
+            <DownloadList downloads={downloads} />
+          )}
+        </>
+      )}
+    </Section>
+  );
 };
 
 export default Downloads;
