0db629f30eed9723463c90c48a381ee8158c7d41
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-05-08 12:44:27 +0200

    section component

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,11 +1,22 @@
 const Section = ({
   children,
   extraStyles = "",
+  changePaddings = "",
 }: {
   children: React.ReactNode;
   extraStyles?: string;
+  changePaddings?: string;
 }) => {
-  return <section className={"px-4 " + extraStyles}>{children}</section>;
+  return (
+    <section
+      className={
+        `px-4 ${changePaddings ? changePaddings : "min-[1440px]:p-24"} ` +
+        extraStyles
+      }
+    >
+      {children}
+    </section>
+  );
 };
 
 export default Section;
