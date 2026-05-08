9fbba0d6b79ecefe2f37b1f25ba7866eb433fbcb
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-05-08 12:32:26 +0200

    products logic

--- features/products/interface.ts
+++ features/products/interface.ts
@@ -1,6 +1,7 @@
 export interface IProduct {
   id: string;
-  photos: string[];
+  photos: { id: string; link: string }[];
+  // photos: string[];
   title: string;
   description: string;
   additionalInformation: string;
@@ -8,4 +9,28 @@ export interface IProduct {
   amount: number;
   isArchived: boolean;
   rate: number;
+}
+
+export interface ICreateProduct {
+  photos: FileList;
+  // photos: File[];
+  title: string;
+  description: string;
+  additionalInformation?: string;
+  price: number;
+  amount?: number;
+}
+
+export interface ICreateProductForm {
+  photos: FileList;
+  // photos: File[];
+  title: string;
+  description: string;
+  additionalInformation?: string;
+  price: number;
+  amount?: number;
+}
+
+export interface IProductFull extends IProduct {
+  reviewCount: number;
 }
--- features/products/requests.ts
+++ features/products/requests.ts
@@ -1,9 +1,58 @@
 import api from "../api/api";
+// import type { ICreateProduct } from "./interface";
 
-export const getProducts = async () => {
-  return (await api.get("/products")).data;
+// export const getProducts = async () => {
+//   return (await api.get("/products")).data;
+// };
+
+// export const getProducts = async (search?: string) => {
+//   return (
+//     await api.get("/products", {
+//       params: { search: search },
+//     })
+//   ).data;
+// };
+
+export const getProducts = async (
+  search?: string,
+  lte?: string,
+  gte?: string,
+  stock?: string,
+  sort?: string,
+) => {
+  return (
+    await api.get("/products", {
+      params: { search, lte, gte, stock, sort },
+    })
+  ).data;
 };
 
 export const getProductById = async (id: string) => {
   return (await api.get("/products/" + id)).data;
+};
+
+export const createProduct = async (data: FormData) => {
+  return (await api.post("/products", data)).data;
+};
+
+export const updateProduct = async ({
+  mutationData,
+  productId,
+}: {
+  mutationData: FormData;
+  productId: string;
+}) => {
+  return (await api.put("/products/" + productId, mutationData)).data;
+};
+
+export const deleteProduct = async (productId: string) => {
+  return (await api.delete("/products/" + productId)).data;
+};
+
+export const archiveProduct = async (productId: string) => {
+  return (await api.patch("/products/archive/" + productId)).data;
+};
+
+export const getProductsStats = async () => {
+  return (await api.get("/products/stats")).data;
 };
