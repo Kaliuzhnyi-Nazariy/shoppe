dd5f5af1f43d57afe8fb3ae1b6fd84b0cd7fd073
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-05-08 12:56:28 +0200

    account page

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -4,7 +4,7 @@ import Section from "../components/Section";
 const Account = () => {
   return (
     <Section extraStyles="mt-6 flex flex-col items-center mb-20.5">
-      <h3>My account</h3>
+      <h3 className="text-xl font-semibold lg:text-[33px]">My account</h3>
 
       <AccountAuth />
     </Section>
