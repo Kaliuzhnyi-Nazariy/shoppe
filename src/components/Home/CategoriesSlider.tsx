371ad44eb5f184cf6bbfadf122843065aab1adb7
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-05-08 12:53:24 +0200

    home page

---
+++ src/components/Home/CategoriesSlider.tsx
@@ -0,0 +1,50 @@
+import { useKeenSlider } from "keen-slider/react";
+
+const CategoriesSlider = () => {
+  const [ref] = useKeenSlider<HTMLDivElement>({
+    slides: {
+      perView: "auto",
+      spacing: 8,
+      origin: "auto",
+    },
+    // loop: true,
+    mode: "free",
+    breakpoints: {
+      "(min-width: 1024px)": {
+        disabled: true,
+      },
+    },
+  });
+
+  const categories = ["Earring", "Necklace", "Rings", "Others"];
+
+  //   return (
+  //     <div ref={ref} className="keen-slider flex text-xs">
+  //       {categories.map((c, idx) => {
+  //         return (
+  //           <div
+  //             className="keen-slider__slide min-w-35 h-10 border border-(--light-gray) rounded-sm flex items-center justify-center"
+  //             key={idx}
+  //           >
+  //             {c}
+  //           </div>
+  //         );
+  //       })}
+  //     </div>
+  //   );
+
+  return (
+    <div ref={ref} className="keen-slider text-xs min-[1024px]:hidden">
+      {categories.map((c, idx) => (
+        <div
+          key={idx}
+          className="keen-slider__slide min-w-35! px-4 h-10 border border-(--light-gray) rounded-sm flex items-center justify-center"
+        >
+          {c}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default CategoriesSlider;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,36 @@
+import { useQuery } from "@tanstack/react-query";
+import CategoriesSlider from "../components/Home/CategoriesSlider";
+import ProductList from "../components/Product/Shop/ProductList";
+import Section from "../components/Section";
+import { getProducts } from "../../features/products/requests";
+import NoProducts from "../components/Product/Shop/NoProducts";
+
 const Home = () => {
-  return <div>Home</div>;
+  const { data = [], isFetching } = useQuery({
+    queryKey: ["getProducts"],
+    queryFn: () => getProducts("", "", "", "true"),
+    retry: false,
+  });
+
+  return (
+    <Section
+      extraStyles={
+        "mb-18 " + `${data.length === 0 ? " flex flex-col h-full flex-1 " : ""}`
+      }
+    >
+      <div
+        className={`${
+          data.length === 0
+            ? " flex flex-col h-full flex-1 justify-center items-center"
+            : ""
+        }`}
+      >
+        <CategoriesSlider />
+        <ProductList data={data} isPending={isFetching} extraStyle="mt-4" />
+        <NoProducts productLength={data.length} isPending={isFetching} />
+      </div>
+    </Section>
+  );
 };
 
 export default Home;
