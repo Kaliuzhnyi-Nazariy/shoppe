1abb7639f1c8241cc7f7285aa001300fffe018da
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-05-08 12:51:06 +0200

    addresses page

---
+++ src/components/Dashboard/Address/Address.tsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import Form from "./Form/Form";
+import type { IAddress } from "../../../../features/address/interface";
+import { AddressItem } from "./AddressItem";
+// import { useQuery } from "@tanstack/react-query";
+// import { getAddresses } from "../../../../features/address/request";
+
+const Address = ({
+  addresses,
+  addressesPending,
+}: // name,
+{
+  addresses: IAddress[];
+  addressesPending: boolean;
+  // name: string;
+}) => {
+  const [addForm, setAddForm] = useState(false);
+
+  return (
+    <>
+      {/* <h5 className="mt-6 text-[16px]">{name} address</h5> */}
+      <button
+        type="button"
+        className="mt-4.5 text-(--accent)"
+        onClick={() => setAddForm(!addForm)}
+      >
+        {addForm ? "CANCEL" : "ADD"}
+      </button>
+
+      {addForm ? (
+        <Form isOpen={addForm} closeUpdateForm={() => setAddForm(false)} />
+      ) : (
+        <>
+          {addressesPending ? (
+            "loading..."
+          ) : (
+            <>
+              {addresses.length === 0 ? (
+                <p className="text-(--dark-gray) mt-4">
+                  You have not set up this type of address yet.
+                </p>
+              ) : (
+                <ul className="flex flex-col gap-4 mt-4">
+                  {addresses.map((a) => {
+                    return <AddressItem address={a} key={a.id} />;
+                  })}
+                </ul>
+              )}
+            </>
+          )}
+        </>
+      )}
+    </>
+  );
+};
+
+export default Address;
---
+++ src/components/Dashboard/Address/AddressItem.tsx
@@ -0,0 +1,102 @@
+import { useState } from "react";
+import type { IAddress } from "../../../../features/address/interface";
+import Form from "./Form/Form";
+import StyledButton from "../../StyledButton";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteAddress } from "../../../../features/address/request";
+import { errorToast, successToast } from "../../toast";
+
+export const AddressItem = ({ address }: { address: IAddress }) => {
+  const [updateFrom, setUpdateForm] = useState(false);
+
+  const queryClient = useQueryClient();
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: () => deleteAddress(address.id),
+    onSuccess() {
+      queryClient.invalidateQueries({ queryKey: ["getAddresses"] });
+      successToast("Address is deleted!");
+    },
+    onError(err) {
+      const error = err as { response?: { data?: { message: string } } };
+      errorToast(error.response?.data?.message || "Something went wrong!");
+    },
+  });
+
+  return (
+    <>
+      {isPending ? (
+        "Loading..."
+      ) : (
+        <>
+          <li className="border border-(--gray) rounded-md p-5">
+            {updateFrom ? (
+              <Form
+                isOpen={updateFrom}
+                data={address}
+                type="update"
+                closeUpdateForm={() => setUpdateForm(false)}
+              />
+            ) : (
+              <>
+                <p>
+                  <span className="text-(--accent)">Country:</span>{" "}
+                  {address.country}
+                </p>
+                <p>
+                  <span className="text-(--accent)">City:</span> {address.city}
+                </p>
+                <p>
+                  <span className="text-(--accent)">Street:</span>{" "}
+                  {address.streetAddress}
+                </p>
+                <p>
+                  <span className="text-(--accent)">Postcode:</span>{" "}
+                  {address.postcode}
+                </p>
+                {address.companyName && (
+                  <p>Company name: {address.companyName}</p>
+                )}
+                <p>
+                  <span className="text-(--accent)">First name:</span>{" "}
+                  {address.firstName}
+                </p>
+                <p>
+                  <span className="text-(--accent)">Last name:</span>{" "}
+                  {address.lastName}
+                </p>
+                <p>
+                  <span className="text-(--accent)">Email:</span>{" "}
+                  {address.email}
+                </p>
+                <p>
+                  <span className="text-(--accent)">Phone:</span>{" "}
+                  {address.phone}
+                </p>
+              </>
+            )}
+            {!updateFrom && (
+              // <button type="button" onClick={() => setUpdateForm(!updateFrom)}>
+              //   Update
+              // </button>
+              <div className="mt-4 flex gap-2 ">
+                <StyledButton
+                  text="Delete"
+                  extraStyles="py-1.5 px-2 "
+                  fn={() => mutate()}
+                  pending={isPending}
+                />
+                <StyledButton
+                  text="Update"
+                  fn={() => setUpdateForm(true)}
+                  type="secondary"
+                  extraStyles="py-1.5 px-2 "
+                />
+              </div>
+            )}
+          </li>
+        </>
+      )}
+    </>
+  );
+};
